3a98de9a524ecbbbbf089578f2094f9c91188cd4
Daniel Beach <dtmhawk@gmail.com>
2022-06-09 11:37:55 +1000

    add travel points for nave mesh

---
+++ Assets/TravelPoint.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TravelPoint : Interactable
+{
+    [SerializeField] Outline outline;
+    [SerializeField] Transform destination;
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    public override void Interact()
+    {
+        NavMeshAgent agent = player.GetComponent<NavMeshAgent>();
+        agent.Warp(destination.position);
+    }
+    private void OnMouseEnter()
+    {
+        show();
+    }
+    private void OnMouseExit()
+    {
+        hide();
+    }
+    // Update is called once per frame
+    public void show()
+    {
+
+        outline.enabled = true;
+    }
+    public void hide()
+    {
+        outline.enabled = false;
+    }
+}
